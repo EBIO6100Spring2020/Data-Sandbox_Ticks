e97947e250f9d2e8c7dba00e5ffbfc8f1962940f
Wynne Moss <wynne.moss@colorado.edu>
2020-01-30 17:12:40 -0700

    Added exploratory plots of prevalence by site, time

--- TickPathogen_02_DataCleanUp.R
+++ TickPathogen_02_DataCleanUp.R
@@ -34,13 +34,13 @@ length(unique(tp$subsampleID))
 length(unique(tp$testingID))
 table(tp$testPathogenName, tp$testResult)
 table(tp$sampleCondition)
-# looks like Borrelia spp have best data; think about grouping? 
+# looks like Borrelia spp have best data; think about using this rather than a specific species 
 #### filter low quality data/errors #####
 tp <- tp %>% filter(!is.na(testPathogenName), sampleCondition == "OK")
 
 
-#### Re-format data #######
-# pathogen results make NAs or numeric
+#### re-format and aggregate data #######
+# pathogen results: make not tested NAs and this column numeric
 levels(tp$testResult) <- c(NA, "0", "1")
 tp$testResult <- as.numeric(as.character(tp$testResult))
 
@@ -54,29 +54,44 @@ tp$Date <- as.Date(tp$collectDate)
 tp$Year <- year(tp$Date)
 tp$Month <- month(tp$Date)
 
+# separate the testingID into other columns
+# some of this ID is redundant with other columns so we'll toss them from table
+tp <- tp %>% separate(col = subsampleID, sep = "\\.", into = c("throw", "throw2", "Taxonomy", "Lifestage"), remove = FALSE) %>% select(-"throw", -"throw2")
+
+
 
 # make tick data in wide format, with each row a single tick, each column a pathogen
 # note new dplyr function pivot_wider() which replaces spread()
 
-tp_wide_cols <- tp %>% group_by(testingID) %>% summarise_at(vars(subsampleID:sampleCondition, Date:Month), first)
+
+# first aggregate the non-pathogen test columns (other covariates)
+tp_wide_cols <- tp %>% group_by(testingID) %>% summarise_at(vars(subsampleID:sampleCondition, Date:Month), first) 
+
+# now re-arrange so that each row is a tick and there are multiple columns for each pathogen ("wide" format), join with other covariates
 tp_wide <- tp %>% pivot_wider(id_cols = testingID, names_from = testPathogenName,values_from = testResult) %>% left_join(tp_wide_cols, by = "testingID")
+
 rm(tp_wide_cols)
 tp_wide <- tp_wide %>% select(-`HardTick DNA Quality`)
 
+
 # aggregate by population
-tp_wide %>% group_by(subsampleID) %>% summarise(Borrelia.Prev = sum(`Borrelia sp.`)/n(),
-                                                Date = first(Date),
-                                                siteID = first(siteID),
-                                                nlcdClass = first(nlcdClass),
-                                                elevation = first(elevation),
-                                                Year = first(Year),
-                                                Month = first(Month)) -> tp_agg
-plot(jitter(Borrelia.Prev) ~ jitter(Month), tp_agg)
+tp_wide %>% group_by(siteID, Date, Taxonomy) %>% 
+  summarise(Borrelia.Prev = sum(`Borrelia sp.`)/n(),
+            nlcdClass = first(nlcdClass),
+            elevation = first(elevation),
+            Year = first(Year),
+            Month = first(Month)) -> tp_agg
+
+#### visualize data ######
+ggplot(data = tp_agg, aes(x=Date, y = Borrelia.Prev, group = siteID))+
+  geom_line(aes(color = siteID))
 
 ggplot(data = tp_agg, aes(x=Year, y = Borrelia.Prev)) + 
-  geom_jitter(aes(group=siteID), alpha = .2)
+  geom_jitter(aes(group=siteID, color = siteID), alpha = .7)
+
 ggplot(data = tp_agg, aes(x=Month, y = Borrelia.Prev)) + 
   geom_jitter(aes(group=siteID), alpha = .2)
+
 ggplot(data = tp_agg, aes(x=nlcdClass, y = Borrelia.Prev)) + 
   geom_violin()
 ggplot(data = tp_agg, aes(x=siteID, y = Borrelia.Prev)) + 
