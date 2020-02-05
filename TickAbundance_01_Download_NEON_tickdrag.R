9c723491b79b0c0f497a731d20faeb791b689cae
Wynne Moss <wynne.moss@colorado.edu>
2020-02-05 16:18:28 -0700

    downloaded tick abundance and taxonomy field data from NEON

--- TickAbundance_01_Download_NEON_tickdrag.R
+++ TickAbundance_01_Download_NEON_tickdrag.R
@@ -2,7 +2,7 @@
 ## Script to download NEON Tick abundance data for EDA
 ## 30 Jan 2020
 ## WEM
-
+library(dplyr)
 library(neonUtilities)
 ### downloading NEON data #####
 # Tick Abundance data are in DP1.10093.001
@@ -12,3 +12,10 @@ stackByTable("data_raw/filesToStack10093/") # merges zips into csv
 
 tickField <- read.csv("data_raw/filesToStack10093/stackedFiles/tck_fielddata.csv")
 head(tickField)
+tickTaxon <- read.csv("data_raw/filesToStack10093/stackedFiles/tck_taxonomyProcessed.csv")
+# tickTaxon %>% filter(family == "") %>%View()
+# head(tickTaxon)
+tickTaxon <- tickTaxon %>% select(namedLocation, plotID, collectDate, sampleID, subsampleID, scientificName,
+                                  acceptedTaxonID, genus, family, subfamily, sexOrAge, individualCount)
+write.csv(tickTaxon, "data_derived/Tick_Abundance_Taxonomy.csv", row.names = FALSE)
+write.csv(tickField, "data_derived/Tick_Abundance_Counts.csv", row.names = FALSE)
