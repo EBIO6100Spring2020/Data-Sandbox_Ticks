537ba5c27407b88458966a339991936f3bbc1035
Melissa Chen <melissachen@cu-tcom-5-10.203.36.185.int.colorado.edu>
2020-02-06 10:13:43 -0700

    don't check size

--- TickAbundance_01_Download_NEON_tickdrag.R
+++ TickAbundance_01_Download_NEON_tickdrag.R
@@ -7,7 +7,7 @@ library(neonUtilities)
 ### downloading NEON data #####
 # Tick Abundance data are in DP1.10093.001
 zipsByProduct(dpID = "DP1.10093.001", site = "all", package = "basic",
-              savepath = "data_raw/") # downloads from NEON as zip
+              savepath = "data_raw/", check.size = F) # downloads from NEON as zip
 stackByTable("data_raw/filesToStack10093/") # merges zips into csv
 
 tickField <- read.csv("data_raw/filesToStack10093/stackedFiles/tck_fielddata.csv")
