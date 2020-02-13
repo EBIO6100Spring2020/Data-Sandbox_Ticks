681a9072bb3c6b9a15d66c8e6d871dd6d4182a6f
Melissa Chen <melissachen@cu-tcom-5-10.203.35.6.int.colorado.edu>
2020-02-13 12:13:40 -0700

    sorry for constant changes; plots and summary stat changes

--- TickPathogen_02_datawrangling_EDA.R
+++ TickPathogen_02_datawrangling_EDA.R
@@ -15,55 +15,85 @@ tck_tax <- read.csv("data_raw/filesToStack10093/stackedFiles/tck_taxonomyProcess
 # Tick pathogen data
 tck_path <- read.csv("data_raw/filesToStack10092/stackedFiles/tck_pathogen.csv", as.is = TRUE)
 
+dir.create("./output/EDA_plots")
+
 
 ###### Tick Field Data ########
 # Check nested-ness of domain, site, and plot
+
+dir.create("./output/EDA_plots/data_summary")
+cat("SUMMARY INFORMATION ",""
+    , file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", sep = "\n")
 # Number of domains:
 length(unique(tck_field$domainID))
+cat("","NUMBER OF DOMAINS: ", length(unique(tck_field$domainID))
+    , file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", sep = "\n", append=TRUE)
 # Number of sites:
 length(unique(tck_field$siteID))
+cat("","NUMBER OF SITES: ", length(unique(tck_field$siteID))
+    , file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", sep = "\n", append=TRUE)
 # Number of plots
 length(unique(tck_field$plotID))
+cat("","NUMBER OF PLOTS: ", length(unique(tck_field$plotID))
+    , file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", sep = "\n", append=TRUE)
 # Number of sampling days
 length(unique(as.Date(tck_field$collectDate)))
+cat("","NUMBER OF SAMPLING DAYS: ", length(unique(as.Date(tck_field$collectDate)))
+    , file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", sep = "\n", append=TRUE)
 # Number of sampling years
 length(unique(year(as.Date(tck_field$collectDate))))
+cat("","SAMPLING YEARS: ", unique(year(as.Date(tck_field$collectDate)))
+    , file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", sep = "\n", append=TRUE)
 
 # Table of domain:site
 tck_field %>%
   mutate(domainID=factor(domainID)) %>%
   mutate(siteID= factor(siteID, levels=unique(siteID))) %>%
   select(domainID, siteID) %>%
-  table()
+  table() %>%
+  write.table(file="./output/EDA_plots/data_summary/tick_field_domain_site_matrix.txt", sep = "\t", quote = FALSE)
+
 # Table of site:plot
 tck_field %>%
   select(plotID, siteID) %>%
-  table()
+  table() %>%
+  write.table(file="./output/EDA_plots/data_summary/tick_field_plot_site_matrix.txt", sep = "\t", quote = FALSE)
 
 ## Looking at coordinate and elevation uncertainty
-tck_field %>%
-  ggplot() +
-  geom_histogram(aes(x=coordinateUncertainty))
+ggsave(file="./output/EDA_plots/data_summary/tick_field_coordinateUncertainty.pdf",
+  tck_field %>%
+    ggplot() +
+    geom_histogram(aes(x=coordinateUncertainty))
+)
+ggsave(file="./output/EDA_plots/data_summary/tick_field_elevationUncertainty.pdf",
 tck_field %>%
   ggplot() +
   geom_histogram(aes(x=elevationUncertainty))
+)
 # I think we can ignore this uncertainty for our model for now.
 
 ## Looking at distribution of sampling areas
+ggsave(file="./output/EDA_plots/data_summary/tick_field_totalSampledArea.pdf",
 tck_field %>%
   ggplot() +
   geom_histogram(aes(x=totalSampledArea))
+)
 
 ## What different types of plots are there?
+cat("","TYPES OF PLOTS:", file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", sep="\n", append=TRUE)
 tck_field %>%
   select(plotType) %>%
-  table()
+  table() %>%
+  capture.output(file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", append=TRUE)
 # They are all distributed; can get rid of this
 
 ## What different types of environments are there?
+cat("","TYPES OF ENVIRONMENTS:", file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", sep="\n", append=TRUE)
 tck_field %>%
   select(nlcdClass) %>%
-  table()
+  table() %>%
+  capture.output(file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", append=TRUE)
+
 # This is potentially important; let's keep this.
 
 ## What sampling conditions are there?
@@ -106,9 +136,11 @@ tck_field %>%
 # I don't think we'll need this.
 
 ## Range in samplingMethods?
+cat("","RANGE IN SAMPLING METHODS:", file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", append=TRUE, sep="\n")
 tck_field %>%
   select(samplingMethod) %>%
-  table()
+  table() %>%
+  capture.output(file="./output/EDA_plots/data_summary/tick_field_summary_info.txt", append=TRUE)
 # Include this column.
 
 # Lastly, for some reason sampleID is an incomplete table. I am going to fill this in.
@@ -168,14 +200,21 @@ tck_field_filt <- tck_field %>%
 
 ##### Tick taxonomy ######
 
+cat("SUMMARY INFO","", file="./output/EDA_plots/data_summary/tick_taxonomy_data_summary_info.txt", sep="\n")
 ## What conditions are there?
+cat("","SAMPLE CONDITIONS:", file="./output/EDA_plots/data_summary/tick_taxonomy_data_summary_info.txt", sep="\n", append=TRUE)
 tck_tax %>% 
   select(sampleCondition) %>%
-  table()
+  table() %>%
+  capture.output(file="./output/EDA_plots/data_summary/tick_taxonomy_data_summary_info.txt", append=TRUE)
 # Just get rid of all non-OKs
+
+## Summary of sex and age
+cat("","SEX AND AGE:", file="./output/EDA_plots/data_summary/tick_taxonomy_data_summary_info.txt", sep="\n", append=TRUE)
 tck_tax %>% 
   select(sexOrAge) %>%
-  table()
+  table() %>%
+  capture.output(file="./output/EDA_plots/data_summary/tick_taxonomy_data_summary_info.txt", append=TRUE)
 
 ## What's up with sex and age?
 tck_tax %>%
@@ -201,9 +240,11 @@ tck_tax %>%
 # Extremely long right tail; mode at 1
 
 ## Let's look at taxonomy
+cat("","FAMILY AND GENUS:", file="./output/EDA_plots/data_summary/tick_taxonomy_data_summary_info.txt", sep="\n", append=TRUE)
 tck_tax %>%
   select(family, genus) %>%
-  table()
+  table() %>%
+  capture.output(file="./output/EDA_plots/data_summary/tick_taxonomy_data_summary_info.txt", append=TRUE)
 # Do the individuals with no famiy HAVE no family, or are they simply not added?
 tck_tax %>%
   filter(family=="")  %>%
@@ -218,9 +259,11 @@ tck_tax %>%
   filter(scientificName=="")
 
 ## How does the rest of the data look, down to genus?
+cat("","GENUS AND SP", file="./output/EDA_plots/data_summary/tick_taxonomy_data_summary_info.txt", sep="\n", append=TRUE)
 tck_tax %>%
   select(genus, specificEpithet) %>%
-  table()
+  table() %>% 
+  capture.output(file="./output/EDA_plots/data_summary/tick_taxonomy_data_summary_info.txt", append=TRUE)
 # Looks good
 
 tck_tax %>%
@@ -273,6 +316,7 @@ tck_tax_filt <- tck_tax %>%
 ###### Tick Pathogen Data #######
 #### Looking at tck pathogen 
 ### Looking at tck taxonomy
+cat("SUMMARY FILE","", file="./output/EDA_plots/data_summary/tick_pathogen_data_summary_info.txt", sep="\n")
 # All plots an samples are found within tck_field? If false, then all included.
 any(!tck_tax$sampleID %in% tck_field$sampleID)
 
@@ -282,7 +326,9 @@ tck_tax %>%
   table()
 
 ## How many tests, and what results?
-table(tck_path$testResult)
+cat("","TEST RESULTS:", file="./output/EDA_plots/data_summary/tick_pathogen_data_summary_info.txt", sep="\n", append=TRUE)
+table(tck_path$testResult) %>%
+  capture.output( file="./output/EDA_plots/data_summary/tick_pathogen_data_summary_info.txt", append=TRUE)
 # We should get rid of all rows with unknown
 
 ## Are all counts just 1?
@@ -290,11 +336,15 @@ table(tck_path$individualCount)
 # Yes; we can get rid of this column since each row is an individual observation
 
 ## What types of conditions are there?
-table(tck_path$sampleCondition)
+cat("","SAMPLING CONDITION:", file="./output/EDA_plots/data_summary/tick_pathogen_data_summary_info.txt", sep="\n", append=TRUE)
+table(tck_path$sampleCondition) %>%
+  capture.output(file="./output/EDA_plots/data_summary/tick_pathogen_data_summary_info.txt", append=TRUE)
 # Most are OK, so let's get rid of everything else.
 
 ## How many diff types of pathogens?
-table(tck_path$testPathogenName, tck_path$testResult)
+cat("","DIFFERENT TYPES OF PATHOGENS", file="./output/EDA_plots/data_summary/tick_pathogen_data_summary_info.txt", sep="\n", append=TRUE)
+table(tck_path$testPathogenName, tck_path$testResult) %>%
+  capture.output(file="./output/EDA_plots/data_summary/tick_pathogen_data_summary_info.txt", append=TRUE)
 unique(tck_path$testPathogenName)
 # What is Borrelia sp. and HardTick DNA Quality?
 
@@ -347,14 +397,12 @@ tck_path_filt <- tck_path %>%
          , batchID, testedBy # random effects
          ) 
 
-
-
-
-###### Plotting data to visualize variables #######
+###### Plotting pathogen data to visualize variables #######
 
 # Now, let's see distribution of pathogens across plots and years
-dir.create("./output/EDA_plots")
-ggsave(filename="./output/EDA_plots/pathogen_vs_plotID.pdf", width=12, height=8,
+dir.create("./output/EDA_plots/tick_pathogen")
+
+ggsave(filename="./output/EDA_plots/tick_pathogen/pathogen_vs_plotID.pdf", width=12, height=8,
 tck_path_filt %>%
   arrange(domainID, siteID) %>%
   mutate(plotID=factor(plotID, levels=unique(plotID))) %>%
@@ -368,14 +416,14 @@ tck_path_filt %>%
   facet_wrap(.~Year, nrow=4)
 )
 
-dir.create("./output/EDA_plots/byPathogen")
+dir.create("./output/EDA_plots/tick_pathogen/byPathogen")
 # Get ordered plotID by domain and site
 plot_order <- tck_path_filt %>%
   arrange(domainID, siteID) %>%
   pull(plotID) %>%
   unique()
 for ( p in unique(tck_path_filt$testPathogenName)) {
-  ggsave(filename = paste0("./output/EDA_plots/byPathogen/",p,".pdf"), width=12, height=8,
+  ggsave(filename = paste0("./output/EDA_plots/tick_pathogen/byPathogen/",p,".pdf"), width=12, height=8,
   tck_path_filt %>%
     filter(testPathogenName==p) %>%
     mutate(Year=year(as.Date(collectDate)), yDay = yday(as.Date(collectDate))) %>%
@@ -419,9 +467,9 @@ for ( p in unique(tck_path_filt$testPathogenName)) {
 #   , heigh=40, width=12
 # )
 
-dir.create("./output/EDA_plots/byPlot")
+dir.create("./output/EDA_plots/tick_pathogen/byPlot")
 for ( p in unique(tck_path_filt$plotID)) {
-  ggsave(filename=paste0("./output/EDA_plots/byPlot/",p,".pdf"), width=12, height=8,
+  ggsave(filename=paste0("./output/EDA_plots/tick_pathogen/byPlot/",p,".pdf"), width=12, height=8,
   tck_path_filt %>%
     filter(plotID==p) %>%
     mutate(Year=year(as.Date(collectDate)), yDay = yday(as.Date(collectDate))) %>%
@@ -441,12 +489,12 @@ for ( p in unique(tck_path_filt$plotID)) {
 )  
 }
 
-dir.create("./output/EDA_plots/byDomain_pathogenvsdate")
+dir.create("./output/EDA_plots/tick_pathogen/byDomain_pathogenvsdate")
 for ( d in unique(tck_path_filt$domainID)) {
   nplots <- tck_path_filt %>%
     filter(domainID==d) %>%
     pull(plotID) %>%unique() %>%length()
-  ggsave(filename=paste0("./output/EDA_plots/byDomain_pathogenvsdate/",d,".pdf"), width=12, height=nplots*2+1,
+  ggsave(filename=paste0("./output/EDA_plots/tick_pathogen/byDomain_pathogenvsdate/",d,".pdf"), width=12, height=nplots*2+1,
          tck_path_filt %>%
            filter(domainID==d) %>%
            mutate(Year=year(as.Date(collectDate)), yDay = yday(as.Date(collectDate))) %>%
